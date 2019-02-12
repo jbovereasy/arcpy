cf8112f506f6754adbc22e2fed65063a881ae446
Baltazar <johnpatrick.baltazar.935@my.csun.edu>
2019-02-12 15:55:03 -0800

    if geographic

--- listFeature.py
+++ listFeature.py
@@ -5,17 +5,17 @@ import arcpy
 
 workEnv = r"C:\Users\jsb13207\devops\GEOG408E\Assignments\1\Assignment_01.gdb"
 arcpy.env.workspace = workEnv
-print "Current directory is {0}".format(arcpy.env.workspace)
+print "Current directory is {0}".format(workEnv)
 
 fList = arcpy.ListFeatureClasses() # list all feature classes
 for x in fList:
    dList = arcpy.Describe(x).shapeType # list feature type: point, shape, etc..
    cList = arcpy.GetCount_management(x) # list num of counts on a feature
    pList = arcpy.Describe(x).spatialReference.type # list if projected or geographic
-   print "{0} feature class is {1} type and has {2} features. It is {3}.".format(x, dList.lower(), cList, pList.lower())
 
-   # if arcpy.Describe(x).spatialReference.type == 'Geographic':
-   #     print "{0} feature class is {1} type and has {2} features. It is not projected".format(x, dList.lower(), cList))
-   # else:
-
+   if pList == 'Geographic':
+      print:"{0} feature class is {1} type and has {2} features. It is not projected."(x, dList.lower(), cList)
+   else:
+      print "{0} feature class is {1} type and has {2} features. It is {3}.".format(x, dList.lower(), cList, pList.lower())
+      
 print "\nEOF"
