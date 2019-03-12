2933fb80ad666c47a8db83b7a5b5333fd038a37e
Baltazar <johnpatrick.baltazar.935@my.csun.edu>
2019-03-12 15:34:44 -0700

    update cursor

--- updateCursor.py
+++ updateCursor.py
@@ -3,17 +3,25 @@ import arcpy
 arcpy.env.workspace = r"C:\<filepath>"
 aFC = "LandParcels"
 
+# Update a specific row on a table
 with arcpy.da.UpdateCursor(aFC, "*") as aC:
     for i in aC:
         if i[2] > 100 and i[3] > 100:
             i[6] = ((i[2] - i[3]) / i[3]) * 100
             aC.updateRow(i)
-            if [6] < 3:
+            if i[6] > -3:
                 i[7] = "A"
+            if i[6] >= 0 and i[6] >= 0:
+                i[7] = "B"
+            if i[6] = 0:
+                i[7] = "C"
+            if i[6] < 3:
+                i[7] = "D"
+            if i[6] < 3:
+                i[7] = "E"
         else:
-            print "goof"
+            print "something goof"
 
 del i
 del aC
-
-print "/n/nEOF"
+print "\n\nEOF"
