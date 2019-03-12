af7a25167ea379f3e65de58399f1b9f6c416b8e8
John Baltazar <johnpatrick.baltazar.935@my.csun.edu>
2019-03-12 16:17:56 -0700

    Update updateCursor.py

--- updateCursor.py
+++ updateCursor.py
@@ -1,27 +1,31 @@
-import arcpy
+import arcpy, datetime
 
-arcpy.env.workspace = r"C:\<filepath>"
+arcpy.env.workspace = r"C:\Users\jsb13207\devops\GEOG408E\Assignments\3\Assignment_03\Assignment_03.gdb"
 aFC = "LandParcels"
 
-# Update a specific row on a table
+OF = open(r"C:\Users\jsb13207\devops\GEOG408E\Assignments\3\Assignment_03\msg.txt", "w")
+
 with arcpy.da.UpdateCursor(aFC, "*") as aC:
     for i in aC:
         if i[2] > 100 and i[3] > 100:
             i[6] = ((i[2] - i[3]) / i[3]) * 100
-            aC.updateRow(i)
-            if i[6] > -3:
+            if i[6] < -3:
                 i[7] = "A"
-            if i[6] >= 0 and i[6] >= 0:
+            if i[6] >= -3 and i[6] <= 0:
                 i[7] = "B"
-            if i[6] = 0:
+            if i[6] == 0:
                 i[7] = "C"
-            if i[6] < 3:
+            if i[6] >= 0 and i[6] <= 3:
                 i[7] = "D"
-            if i[6] < 3:
+            if i[6] > 3:
                 i[7] = "E"
-        else:
-            print "something goof"
-
+            aC.updateRow(i)
+            
 del i
 del aC
-print "\n\nEOF"
+
+print datetime.datetime.now()
+OF.write(str(datetime.datetime.now()))
+OF.close()
+
+print "\nEOF\n"
