dd29edbbb33951654652ca743896054cf44bc563
Baltazar <johnpatrick.baltazar.935@my.csun.edu>
2019-03-27 13:42:19 -0700

    updated untitled

--- untitled.py
+++ untitled.py
@@ -1,25 +1,28 @@
 import arcpy
-arcpy.env.workspace = r"C:\Users\jsb13207\devops\GEOG408E\Assignments\4\Assignment_04\Assignment_04.gdb"
 
+fPath = r"C:\Users\jsb13207\devops\GEOG408E\Assignments\4\Assignment_04"
+cGdb = "Assignment_04.gdb"
+nGdb = "PCS_Data.gdb"
+arcpy.env.workspace = fPath + "\\" + cGdb
 
-def newGdb():
-    arcpy.CreateFileGDB_management(r"C:\Users\jsb13207\devops\GEOG408E\Assignments\4\Assignment_04", "PCS_Data.gdb")
-    arcpy.env.workspace = r"C:\Users\jsb13207\devops\GEOG408E\Assignments\4\Assignment_04\PCS_Data.gdb"
+# def newGdb():
+#     arcpy.CreateFileGDB_management(r"C:\Users\jsb13207\devops\GEOG408E\Assignments\4\Assignment_04", "PCS_Data.gdb")
+#     arcpy.env.workspace = r"C:\Users\jsb13207\devops\GEOG408E\Assignments\4\Assignment_04\PCS_Data.gdb"
     
 def checkProjection():
     fList = arcpy.ListFeatureClasses()
     for i in fList:
         desc = arcpy.Describe(i)
         pList = desc.spatialReference.type
-        rName = i+"_P"
+        rName = fPath + "\\" + nGdb + "\\" + i + "_P"
         if pList == 'Geographic':
             if arcpy.Exists(rName):
                 arcpy.Delete_management(rName)
-            arcpy.Project_management(i, rName, arcpy.SpatialReference(26917))
+            arcpy.Project_management(i, rName, arcpy.SpatialReference(26945))
         else:
-            print "nothing happened"
+            print "you goofed something" 
 
-newGdb()
+# newGdb()
 checkProjection()
 
 print "EOF!!!\n"
