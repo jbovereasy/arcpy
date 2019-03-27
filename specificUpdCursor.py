fe8a28116be339fb42431368fc21cadcd1d393f8
John Baltazar <johnpatrick.baltazar.935@my.csun.edu>
2019-03-26 17:48:35 -0700

    Create specificUpdCursor.py

---
+++ specificUpdCursor.py
@@ -0,0 +1,21 @@
+import arcpy, datetime
+
+arcpy.env.workspace = r"C:\Users\jsb13207\devops\GEOG408E\Exam\Exam-03\Exam_3.gdb"
+OF = open(r"C:\Users\jsb13207\devops\GEOG408E\Exam\Exam-03\msg.txt", "w")
+
+aFC = "LandParcels"
+aList = ["TaxVal06", "TaxVal00", "Per_Change"]
+aQuery = '(TaxVal00 > 0 AND TaxVal06 > 0) AND (UseCode = \'A\' OR UseCode = \'B\')'
+
+with arcpy.da.UpdateCursor(aFC, aList, aQuery) as aC:
+    for i in aC:
+        i[2] = ((i[0] - float(i[1]))/i[1])*100.00
+        aC.updateRow(i)
+
+del i
+del aC
+
+OF.write("updated!\n" + str(datetime.datetime.now()))
+OF.close()
+print datetime.datetime.now()
+print "EOF\n"
