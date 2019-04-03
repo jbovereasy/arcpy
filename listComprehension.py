2a5473dec0bb60ec259f87350ff0de579e958bbd
John Baltazar <johnpatrick.baltazar.935@my.csun.edu>
2019-04-02 18:13:27 -0700

    Create listComprehension.py

---
+++ listComprehension.py
@@ -0,0 +1,9 @@
+listA = ["streets", "Roads", "RAILS", "Highways"]
+print listA
+
+listB = [i.lower() for i in listA]
+print listB
+
+list1 = [7, 11, 13, 44]
+list2 = [i**2 for i in list1]
+print list2
