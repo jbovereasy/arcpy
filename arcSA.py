17f8e7f58b44c64192b7e9a78a483e90a0724fde
John Baltazar <johnpatrick.baltazar.935@my.csun.edu>
2019-04-11 18:27:10 -0700

    Update arcSA.py

--- arcSA.py
+++ arcSA.py
@@ -1,18 +1,26 @@
-import arcpy
+import arcpy, numpy
 from arcpy.sa import * 
 
 arcpy.env.workspace = r"C:\Users\jsb13207\devops\GEOG408E\Week_12\Example_Data.gdb"
 arcpy.CheckOutExtension("Spatial")
 arcpy.env.overwriteOutput = False # if true, it will overwrite everything
 
-rList = arcpy.ListRasters()
+def rasterNumpy():
+    aR = arcpy.Raster("DEM")
+    aRray = arcpy.RasterToNumPyArray(aR)
+    print aRray
 
-for i in rList:
-    rasterObj = arcpy.Raster(i)
-    print "{} has RAT {} ".format(rasterObj.name, rasterObj.hasRAT)
-    if rasterObj.name == "DEM":
-        aSlope = Slope(rasterObj, "PERCENT_RISE")
-        aSlope.save("DEM_S")
-        print "Slope created"
+def slope(): 
+    rList = arcpy.ListRasters()
+    for i in rList:
+        rasterObj = arcpy.Raster(i)
+        print "{} has RAT {} ".format(rasterObj.name, rasterObj.hasRAT)
+        if rasterObj.name == "DEM":
+            aSlope = Slope(rasterObj, "PERCENT_RISE")
+            aSlope.save("DEM_Slope")
+            print "Slope created"
+
+rasterNumpy()
+slope()
 
 print "EOF\n"    
